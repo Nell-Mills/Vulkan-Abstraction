3f36f32dc2f7c59da6d2b9ff32b1f8ea1b390631
Nell Mills <nell@mad-cat.co.uk>
2025-09-14 17:51:27 +0100

    Initial commit.

---
+++ Demo/VKA-Demo.c
@@ -0,0 +1,6 @@
+#include "../Vulkan-Abstraction.h"
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
---
+++ Vulkan-Abstraction.h
@@ -0,0 +1,8 @@
+#ifndef VULKAN_ABSTRACTION_H
+#define VULKAN_ABSTRACTION_H
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <Volk/volk.h>
+
+#endif
